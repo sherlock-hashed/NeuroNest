ecb32e59ef59564512af979991e15f7b6b63e091
sherlock-hashed <varadparate@gmail.com>
2025-09-04 01:00:29 +0530

    Added runtime edge

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -69,8 +69,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { CreateNewUser, GenerateCourse, GenerateNotes, GenerateStudyTypeContent, helloWorld } from "@/inngest/functions";
+export const runtime = 'edge';
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming:'allow',
   functions: [helloWorld, CreateNewUser, GenerateCourse, GenerateNotes, GenerateStudyTypeContent],
 });
